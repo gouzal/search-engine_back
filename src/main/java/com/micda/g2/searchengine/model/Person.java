7eb78b981f3fe34eab30cd71cb7a1ed17ba8739b
Larbi Gouzal <larbi-inco@live.fr>
2019-02-21 15:59:47 -0800

    add Person Model

---
+++ src/main/java/com/micda/g2/searchengine/model/Person.java
@@ -0,0 +1,73 @@
+package com.micda.g2.searchengine.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Getter
+	@Setter
+	protected int id;
+	@Getter
+	@Setter
+	@NotEmpty
+	@NotNull
+	protected String firstName;
+	@Getter
+	@Setter
+	@NotEmpty
+	@NotNull
+	protected String lastName;
+	@Getter
+	@Setter
+	@NotEmpty
+	@NotNull
+	protected char gendre;
+	@Getter
+	@Setter
+	@NotEmpty
+	@NotNull
+	protected String email;
+	@Getter	
+	@Setter
+	@NotEmpty
+	@NotNull
+	protected String password;
+	@Getter
+	@Setter
+	@NotEmpty
+	@NotNull
+	protected Date birthDate;
+	@Getter
+	@Setter
+	@NotEmpty
+	@NotNull
+	protected boolean isValide=false;
+	@Getter
+	@Setter
+	@NotEmpty
+	@NotNull
+	protected boolean isActive=false;
+	
+	
+}
