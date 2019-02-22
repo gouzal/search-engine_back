31c3902bd19dac36859ad39a970e83dc8366dc38
Larbi Gouzal <larbi-inco@live.fr>
2019-02-21 16:08:47 -0800

    add Employee Model

---
+++ src/main/java/com/micda/g2/searchengine/model/Employee.java
@@ -0,0 +1,16 @@
+package com.micda.g2.searchengine.model;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString(callSuper=true, includeFieldNames=true)
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee extends Person {
+
+	private static final long serialVersionUID = 1L;
+
+}
