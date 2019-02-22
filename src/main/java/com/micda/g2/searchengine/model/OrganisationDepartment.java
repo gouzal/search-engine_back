675a7ae39ff874d15e4d384a681d7e2e5be03048
Larbi Gouzal <larbi-inco@live.fr>
2019-02-21 16:47:26 -0800

    add intermidate OrganisationDepartment Model

---
+++ src/main/java/com/micda/g2/searchengine/model/OrganisationDepartment.java
@@ -0,0 +1,37 @@
+package com.micda.g2.searchengine.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrganisationDepartment {
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Id
+	@Setter
+	@Getter
+	private int id;
+	@Setter
+	@Getter
+	@OneToMany(cascade=CascadeType.ALL,orphanRemoval=true)
+	@JoinColumn(name="organisation_id")
+	private Organisation organisation;
+	@Setter
+	@Getter
+	@OneToMany(cascade=CascadeType.ALL,orphanRemoval=true)
+	@JoinColumn(name="department_id")
+	private Department department;
+}
