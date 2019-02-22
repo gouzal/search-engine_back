9994c6af633a1a6f12a94cba80618512ae12a83d
Larbi Gouzal <larbi-inco@live.fr>
2019-02-21 16:16:07 -0800

    add Professor Model

---
+++ src/main/java/com/micda/g2/searchengine/model/Professor.java
@@ -0,0 +1,20 @@
+package com.micda.g2.searchengine.model;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString(callSuper=true, includeFieldNames=true)
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class Professor extends Person {
+	private static final long serialVersionUID = 4097270904386507290L;
+
+	@Getter
+	@Setter
+	private String grade;
+}
