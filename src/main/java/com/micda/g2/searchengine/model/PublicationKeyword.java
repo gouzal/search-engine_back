69c467613b0dabb6fcacab592a1948846c16ec81
Larbi Gouzal <larbi-inco@live.fr>
2019-02-21 17:12:54 -0800

    add PublicationKeyword Model

---
+++ src/main/java/com/micda/g2/searchengine/model/PublicationKeyword.java
@@ -0,0 +1,33 @@
+package com.micda.g2.searchengine.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.FetchMode;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class PublicationKeyword {
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Id
+	@Getter
+	@Setter
+	private int id;
+	@Getter
+	@Setter
+	@OneToMany(cascade=CascadeType.ALL, orphanRemoval=true,fetch=FetchType.LAZY	)
+	@JoinColumn(name="publication_id")
+	private Publication publication;
+	@Getter
+	@Setter
+	@OneToMany(cascade=CascadeType.ALL, orphanRemoval=true,fetch=FetchType.LAZY	)
+	@JoinColumn(name="keyword_id")
+	private Keyword keyword;
+}
