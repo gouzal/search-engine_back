b7fffa449b163a02c7ab84c37552b9558d7394c8
Larbi Gouzal <larbi-inco@live.fr>
2019-02-21 16:12:48 -0800

    add model Officer

---
+++ src/main/java/com/micda/g2/searchengine/model/Officer.java
@@ -0,0 +1,23 @@
+package com.micda.g2.searchengine.model;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString(callSuper=true, includeFieldNames=true)
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class Officer extends Employee {
+	private static final long serialVersionUID = 1L;
+	
+	@Getter
+	@Setter
+	protected String role;
+	
+	
+
+}
