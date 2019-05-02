f3bae43dadf35371d874f13f6745d8a0f98dbe8c
Larbi Gouzal <you@example.com>
2019-05-02 23:36:34 +0100

    add Api interfaces

---
+++ src/main/java/com/micda/g2/searchengine/rest/IPublicationApi.java
@@ -0,0 +1,32 @@
+package com.micda.g2.searchengine.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.micda.g2.searchengine.model.Publication;
+
+@RestController
+public interface IPublicationApi {
+	@GetMapping(value = "/{id}")
+	public Publication get(@PathVariable("id") int id);
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Publication add(@RequestParam(value = "name") String name);
+
+	@PutMapping(value = "/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public void update(@PathVariable("id") int id, @RequestBody Publication publication);
+
+	@DeleteMapping(value = "/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public void delete(@PathVariable("id") int id);
+}
